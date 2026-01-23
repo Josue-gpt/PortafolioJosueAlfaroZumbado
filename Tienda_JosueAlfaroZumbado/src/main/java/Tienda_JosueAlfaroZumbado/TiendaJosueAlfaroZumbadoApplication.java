9855ff600377008e781bd269441945dc2850126f
Josue-gpt <jalfaroz2006@gmail.com>
2026-01-23 08:58:15 -0600

    Semana2

---
+++ Tienda_JosueAlfaroZumbado/src/main/java/Tienda_JosueAlfaroZumbado/TiendaJosueAlfaroZumbadoApplication.java
@@ -0,0 +1,13 @@
+package Tienda_JosueAlfaroZumbado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJosueAlfaroZumbadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJosueAlfaroZumbadoApplication.class, args);
+	}
+
+}
---
+++ Tienda_JosueAlfaroZumbado/src/test/java/Tienda_JosueAlfaroZumbado/TiendaJosueAlfaroZumbadoApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_JosueAlfaroZumbado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaJosueAlfaroZumbadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
