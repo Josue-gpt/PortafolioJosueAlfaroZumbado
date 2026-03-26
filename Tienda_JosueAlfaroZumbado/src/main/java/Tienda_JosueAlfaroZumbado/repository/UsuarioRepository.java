d5c377229432dbef680ba11ea9c754a98502a198
Josue-gpt <jalfaroz2006@gmail.com>
2026-03-25 18:13:42 -0600

    Semana10

---
+++ Tienda_JosueAlfaroZumbado/src/main/java/Tienda_JosueAlfaroZumbado/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Tienda_JosueAlfaroZumbado.repository;
+import Tienda_JosueAlfaroZumbado.domain.Usuario;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    
+    public Optional<Usuario> findByUsernameAndActivoTrue(String username);
+    
+}
