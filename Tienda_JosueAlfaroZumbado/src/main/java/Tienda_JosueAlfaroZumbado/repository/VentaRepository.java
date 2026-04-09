71d881318b92c345717d13d099ea373496d8b67b
Josue-gpt <jalfaroz2006@gmail.com>
2026-04-08 19:29:46 -0600

    Semana12
    Carrito

---
+++ Tienda_JosueAlfaroZumbado/src/main/java/Tienda_JosueAlfaroZumbado/repository/VentaRepository.java
@@ -0,0 +1,8 @@
+package Tienda_JosueAlfaroZumbado.repository;
+
+import Tienda_JosueAlfaroZumbado.domain.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaRepository extends JpaRepository<Venta, Integer>{
+    
+}
