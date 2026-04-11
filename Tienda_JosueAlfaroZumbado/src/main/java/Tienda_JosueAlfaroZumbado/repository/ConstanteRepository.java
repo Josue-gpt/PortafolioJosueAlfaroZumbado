51bd67d8d8685d0c6b2eefb248250a368ed67f37
Josue-gpt <jalfaroz2006@gmail.com>
2026-04-11 16:32:02 -0600

    Semana13
    Ya funciona darle roles otros usuarios

---
+++ Tienda_JosueAlfaroZumbado/src/main/java/Tienda_JosueAlfaroZumbado/repository/ConstanteRepository.java
@@ -0,0 +1,11 @@
+package Tienda_JosueAlfaroZumbado.repository;
+
+import Tienda_JosueAlfaroZumbado.domain.Constante;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConstanteRepository extends JpaRepository<Constante,Integer> {
+    
+    public Optional<Constante> findByAtributo(String atributo);
+    
+}
